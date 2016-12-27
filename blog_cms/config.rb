182c54efae488a7792f873260ba9484c0032dde2
tsundoa <tsunoda@qures.jp>
2016-12-27 13:57:37 +0900

    setup commit(bootstrap-sass)

--- blog_cms/config.rb
+++ blog_cms/config.rb
@@ -1,4 +1,5 @@
 require 'compass/import-once/activate'
+require 'bootstrap-sass'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -6,7 +7,7 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
